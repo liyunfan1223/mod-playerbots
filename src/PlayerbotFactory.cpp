36bb6d478629cb3daec7cfdb2722c52ef603c1e7
Yunfan Li <lyf_liyunfan@sina.com>
2023-07-24 19:52:13 +0800

    clear default race/class skills when randomizing

--- src/PlayerbotFactory.cpp
+++ src/PlayerbotFactory.cpp
@@ -669,9 +669,17 @@ void PlayerbotFactory::ClearSkills()
     {
         bot->SetSkill(tradeSkills[i], 0, 0, 0);
     }
-
     bot->SetUInt32Value(PLAYER_SKILL_INDEX(0), 0);
     bot->SetUInt32Value(PLAYER_SKILL_INDEX(1), 0);
+    // unlearn default race/class skills
+    PlayerInfo const* info = sObjectMgr->GetPlayerInfo(bot->getRace(), bot->getClass());
+    for (PlayerCreateInfoSkills::const_iterator itr = info->skills.begin(); itr != info->skills.end(); ++itr)
+    {
+        uint32 skillId = itr->SkillId;
+        if (!bot->HasSkill(skillId))
+            continue;
+        bot->SetSkill(skillId, 0, 0, 0);
+    }
 }
 
 void PlayerbotFactory::ClearEverything()
