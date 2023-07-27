74293912ecdaf57849b49ea1dc25cf10da1d96ec
Yunfan Li <lyf_liyunfan@sina.com>
2023-07-28 01:18:35 +0800

    optimize performance by increase cure check interval

--- src/strategy/values/PartyMemberToDispel.h
+++ src/strategy/values/PartyMemberToDispel.h
@@ -14,7 +14,7 @@ class Unit;
 class PartyMemberToDispel : public PartyMemberValue, public Qualified
 {
 	public:
-        PartyMemberToDispel(PlayerbotAI* botAI, std::string const name = "party member to dispel") : PartyMemberValue(botAI, name), Qualified() { }
+        PartyMemberToDispel(PlayerbotAI* botAI, std::string const name = "party member to dispel") : PartyMemberValue(botAI, name, 2), Qualified() { }
 
     protected:
         Unit* Calculate() override;
