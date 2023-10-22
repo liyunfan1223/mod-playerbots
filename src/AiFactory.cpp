618f2cd95e2bea93ba6835b83259924db5cd0c4b
Yunfan Li <lyf_liyunfan@sina.com>
2023-10-22 17:00:50 +0800

    shaman bdps

--- src/AiFactory.cpp
+++ src/AiFactory.cpp
@@ -302,7 +302,7 @@ void AiFactory::AddDefaultCombatStrategies(Player* player, PlayerbotAI* const fa
             else if (tab == 2)
                 engine->addStrategies("heal", "bmana", nullptr);
             else
-                engine->addStrategies("melee", "melee aoe", "bmana", "threat", nullptr);
+                engine->addStrategies("melee", "melee aoe", "bdps", "threat", nullptr);
 
             engine->addStrategies("dps assist", "cure", "totems", nullptr);
             break;
