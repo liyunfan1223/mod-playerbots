290a6bad71cbda683f2dfd346a89dfaaa6f94288
Yunfan Li <lyf_liyunfan@sina.com>
2023-12-24 14:59:13 +0800

    Optimize talents initialization

--- src/PlayerbotFactory.cpp
+++ src/PlayerbotFactory.cpp
@@ -799,7 +799,9 @@ void PlayerbotFactory::InitTalentsTree(bool increment/*false*/, bool use_templat
 {
     uint32 specNo;
     uint8 cls = bot->getClass();
-    if (increment && bot->GetFreeTalentPoints() <= 2) {
+    std::map<uint8, uint32> tabs = AiFactory::GetPlayerSpecTabs(bot);
+    uint32 total_tabs = tabs[0] + tabs[1] + tabs[2];
+    if (increment && bot->GetFreeTalentPoints() <= 2 && total_tabs != 0) {
         specNo = AiFactory::GetPlayerSpecTab(bot);
     } else {
         uint32 point = urand(0, 100);
