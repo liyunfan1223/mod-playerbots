600228e3fd2e04c3509312309959cfafc151c380
Yunfan Li <lyf_liyunfan@sina.com>
2024-01-06 17:53:11 +0800

    Warrior spell cleave

--- src/strategy/warrior/GenericWarriorStrategy.cpp
+++ src/strategy/warrior/GenericWarriorStrategy.cpp
@@ -47,6 +47,7 @@ void WarrirorAoeStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
         new NextAction("sweeping strikes", ACTION_HIGH + 3),
         new NextAction("bladestorm", ACTION_HIGH + 3),
         new NextAction("whirlwind", ACTION_HIGH + 2),
+        new NextAction("cleave", ACTION_HIGH + 2),
         nullptr)));
     triggers.push_back(new TriggerNode("shockwave on snare target", NextAction::array(0, new NextAction("shockwave on snare target", ACTION_HIGH + 5), nullptr)));
     triggers.push_back(new TriggerNode("high rage available", NextAction::array(0, new NextAction("whirlwind", ACTION_HIGH + 10), nullptr)));
