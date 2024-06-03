dfe093318938959ed767313cae7b0e6276366bfd
Yunfan Li <lyf_liyunfan@sina.com>
2024-06-03 23:12:57 +0800

    [Initialization] No pet talents reset

--- src/PlayerbotFactory.cpp
+++ src/PlayerbotFactory.cpp
@@ -587,7 +587,7 @@ void PlayerbotFactory::InitPetTalents()
         // LOG_INFO("playerbots", "{} init pet talents failed with petTalentType < 0({})", bot->GetName().c_str(), pet_family->petTalentType);
         return;
     }
-    pet->resetTalents();
+    // pet->resetTalents();
     std::unordered_map<uint32, std::vector<TalentEntry const*> > spells;
     for (uint32 i = 0; i < sTalentStore.GetNumRows(); ++i)
     {
