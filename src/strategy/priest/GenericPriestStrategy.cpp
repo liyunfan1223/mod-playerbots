b81ccfa8295ca414d034d64e4900328c5b564b4c
Yunfan Li <lyf_liyunfan@sina.com>
2024-09-09 22:34:16 +0800

    Reduce inner focus usage

--- src/strategy/priest/GenericPriestStrategy.cpp
+++ src/strategy/priest/GenericPriestStrategy.cpp
@@ -31,6 +31,8 @@ void GenericPriestStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
     // NextAction("inner focus", 42.0f), nullptr))); triggers.push_back(new TriggerNode("medium mana",
     // NextAction::array(0, new NextAction("symbol of hope", ACTION_EMERGENCY), nullptr))); triggers.push_back(new
     // TriggerNode("low mana", NextAction::array(0, new NextAction("consume magic", 10.0f), nullptr)));
+    // triggers.push_back(
+    //     new TriggerNode("inner focus", NextAction::array(0, new NextAction("inner focus", 42.0f), nullptr)));
     triggers.push_back(new TriggerNode("critical health", NextAction::array(0, new NextAction("desperate prayer",
         ACTION_HIGH + 5), nullptr))); 
     // triggers.push_back(new TriggerNode("enemy is close", NextAction::array(0, new
@@ -43,7 +45,10 @@ void GenericPriestStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
         new TriggerNode("low health", NextAction::array(0, new NextAction("power word: shield", ACTION_HIGH), NULL)));
 
     triggers.push_back(
-        new TriggerNode("medium mana", NextAction::array(0, new NextAction("shadowfiend", ACTION_HIGH + 1), NULL)));
+        new TriggerNode("medium mana", 
+            NextAction::array(0, 
+                new NextAction("shadowfiend", ACTION_HIGH + 2),
+                new NextAction("inner focus", ACTION_HIGH + 1), nullptr)));
 
     triggers.push_back(
         new TriggerNode("low mana", NextAction::array(0, new NextAction("hymn of hope", ACTION_HIGH), NULL)));
@@ -72,8 +77,6 @@ void PriestCureStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
 
 void PriestBoostStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
 {
-    triggers.push_back(
-        new TriggerNode("inner focus", NextAction::array(0, new NextAction("inner focus", 42.0f), nullptr)));
     triggers.push_back(
         new TriggerNode("power infusion", NextAction::array(0, new NextAction("power infusion", 41.0f), nullptr)));
     triggers.push_back(new TriggerNode("boost", NextAction::array(0, new NextAction("shadowfiend", 20.0f), nullptr)));
