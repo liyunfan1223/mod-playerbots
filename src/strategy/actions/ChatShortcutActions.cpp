25c46037cdd5c5872b63b83e16de26386a823ebc
Yunfan Li <lyf_liyunfan@sina.com>
2024-09-10 20:45:46 +0800

    Reset prioritized targets

--- src/strategy/actions/ChatShortcutActions.cpp
+++ src/strategy/actions/ChatShortcutActions.cpp
@@ -35,7 +35,7 @@ bool FollowChatShortcutAction::Execute(Event event)
     // botAI->Reset();
     botAI->ChangeStrategy("+follow,-passive,-grind,-move from group", BOT_STATE_NON_COMBAT);
     botAI->ChangeStrategy("-follow,-passive,-grind,-move from group", BOT_STATE_COMBAT);
-    botAI->GetAiObjectContext()->GetValue<GuidVector>("prioritized targets")->Set({});
+    botAI->GetAiObjectContext()->GetValue<GuidVector>("prioritized targets")->Reset();
 
     PositionMap& posMap = context->GetValue<PositionMap&>("position")->Get();
     PositionInfo pos = posMap["return"];
