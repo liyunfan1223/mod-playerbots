b59c87871f204e206cd9748495a672f5c8aee915
Bobblybook <bobblybook@gmail.com>
2024-10-01 18:27:06 +1000

    Indentation and misc cleanup

--- src/strategy/dungeons/wotlk/utgardekeep/UtgardeKeepMultipliers.cpp
+++ src/strategy/dungeons/wotlk/utgardekeep/UtgardeKeepMultipliers.cpp
@@ -6,7 +6,7 @@
 
 float PrinceKelesethMultiplier::GetValue(Action* action)
 {
-	Unit* boss = AI_VALUE2(Unit*, "find target", "prince keleseth");
+    Unit* boss = AI_VALUE2(Unit*, "find target", "prince keleseth");
     if (!boss)
     {
         return 1.0f;
@@ -15,7 +15,7 @@ float PrinceKelesethMultiplier::GetValue(Action* action)
     {
         return 0.0f;
     }
-	return 1.0f;
+    return 1.0f;
 }
 float SkarvaldAndDalronnMultiplier::GetValue(Action* action)
 {
