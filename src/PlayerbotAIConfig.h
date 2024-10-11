158d923c15d1bb062c465261ff091e2f4976c8de
bash <hermensb@gmail.com>
2024-10-11 21:20:43 +0000

    Autoscale latency modifier

--- src/PlayerbotAIConfig.h
+++ src/PlayerbotAIConfig.h
@@ -266,10 +266,8 @@ public:
     bool botActiveAloneSmartScale;
     uint32 botActiveAloneSmartScaleWhenMinLevel;
     uint32 botActiveAloneSmartScaleWhenMaxLevel;
-
-    uint32 enablePrototypePerformanceDiff;
-    uint32 diffWithPlayer;
-    uint32 diffEmpty;
+    uint32 botActiveAloneSmartScaleDiffWithPlayer;
+    uint32 botActiveAloneSmartScaleDiffEmpty;
 
     bool freeMethodLoot;
     int32 lootRollLevel;
