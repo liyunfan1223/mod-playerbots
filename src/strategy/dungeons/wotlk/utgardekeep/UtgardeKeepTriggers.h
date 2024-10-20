a8842b61c5a520123d4c1241c65eb36375ce8371
Bobblybook <bobblybook@gmail.com>
2024-10-20 16:17:57 +1100

    Update UtgardeKeepTriggers.h

--- src/strategy/dungeons/wotlk/utgardekeep/UtgardeKeepTriggers.h
+++ src/strategy/dungeons/wotlk/utgardekeep/UtgardeKeepTriggers.h
@@ -8,35 +8,25 @@
 
 enum UtgardeKeepIDs
 {
-    SPELL_SUMMON_VALKYR             = 42912,
-    SPELL_RESURRECTION_BEAM         = 42857,
-    SPELL_RESURRECTION_BALL         = 42862,
-    SPELL_RESURRECTION_HEAL         = 42704,
-    SPELL_INGVAR_TRANSFORM          = 42796,
+    // Prince Keleseth
+    SPELL_FROST_TOMB               = 48400,
+    NPC_FROST_TOMB                 = 23965,
 
+    // Ingvar the Plunderer
     SPELL_STAGGERING_ROAR_N         = 42708,
     SPELL_STAGGERING_ROAR_H         = 59708,
-    SPELL_CLEAVE                    = 42724,
     SPELL_SMASH_N                   = 42669,
     SPELL_SMASH_H                   = 59706,
-    SPELL_ENRAGE_N                  = 42705,
-    SPELL_ENRAGE_H                  = 59707,
-
     SPELL_DREADFUL_ROAR_N           = 42729,
     SPELL_DREADFUL_ROAR_H           = 59734,
     SPELL_WOE_STRIKE_N              = 42730,
     SPELL_WOE_STRIKE_H              = 59735,
     SPELL_DARK_SMASH                = 42723,
-    SPELL_SHADOW_AXE                = 42749,
-
-    DEBUFF_FROST_TOMB               = 48400,
 };
 
 #define SPELL_STAGGERING_ROAR       DUNGEON_MODE(bot, SPELL_STAGGERING_ROAR_N, SPELL_STAGGERING_ROAR_H)
 #define SPELL_DREADFUL_ROAR         DUNGEON_MODE(bot, SPELL_DREADFUL_ROAR_N, SPELL_DREADFUL_ROAR_H)
-#define SPELL_WOE_STRIKE            DUNGEON_MODE(bot, SPELL_WOE_STRIKE_N, SPELL_WOE_STRIKE_H)
 #define SPELL_SMASH                 DUNGEON_MODE(bot, SPELL_SMASH_N, SPELL_SMASH_H)
-#define SPELL_ENRAGE                DUNGEON_MODE(bot, SPELL_ENRAGE_N, SPELL_ENRAGE_H)
 
 class KelesethFrostTombTrigger : public Trigger
 {
