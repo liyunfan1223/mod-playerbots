7c8563ef7d00a251a2ede0723f8e35b967f8c434
avirar <luke.thorpe@gmail.com>
2024-12-18 16:20:06 +1100

    Update RaidUlduarTriggers.h

--- src/strategy/raids/ulduar/RaidUlduarTriggers.h
+++ src/strategy/raids/ulduar/RaidUlduarTriggers.h
@@ -7,7 +7,9 @@
 #include "RaidUlduarBossHelper.h"
 #include "Trigger.h"
 
-
+//
+// Flame Levi
+//
 class FlameLeviathanOnVehicleTrigger : public Trigger
 {
 public:
@@ -19,6 +21,58 @@ class FlameLeviathanVehicleNearTrigger : public Trigger
 {
 public:
     FlameLeviathanVehicleNearTrigger(PlayerbotAI* ai) : Trigger(ai, "flame leviathan vehicle near") {}
+    bool IsActive() override;
+};
+
+//
+// Razorscale
+//
+class RazorscaleFlyingAloneTrigger : public Trigger
+{
+public:
+    RazorscaleFlyingAloneTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale flying alone") {}
+    bool IsActive() override;
+};
+
+class RazorscaleDevouringFlamesTrigger : public Trigger
+{
+public:
+    RazorscaleDevouringFlamesTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale avoid devouring flames") {}
+    bool IsActive() override;
+};
+
+class RazorscaleAvoidSentinelTrigger : public Trigger
+{
+public:
+    RazorscaleAvoidSentinelTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale avoid sentinel") {}
+    bool IsActive() override;
+};
+
+class RazorscaleAvoidWhirlwindTrigger : public Trigger
+{
+public:
+    RazorscaleAvoidWhirlwindTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale avoid whirlwind") {}
+    bool IsActive() override;
+};
+
+class RazorscaleGroundedTrigger : public Trigger
+{
+public:
+    RazorscaleGroundedTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale grounded") {}
+    bool IsActive() override;
+};
+
+class RazorscaleHarpoonAvailableTrigger : public Trigger
+{
+public:
+    RazorscaleHarpoonAvailableTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale harpoon trigger") {}
+    bool IsActive() override;
+};
+
+class RazorscaleFuseArmorTrigger : public Trigger
+{
+public:
+    RazorscaleFuseArmorTrigger(PlayerbotAI* ai) : Trigger(ai, "razorscale fuse armor trigger") {}
     bool IsActive() override;
 };
 
