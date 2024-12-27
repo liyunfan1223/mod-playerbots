aa7da4eef42004ba75516b314a7d443d1d3a84b2
avirar <luke.thorpe@gmail.com>
2024-12-28 10:38:25 +1100

    Vigilance

--- src/strategy/warrior/WarriorActions.cpp
+++ src/strategy/warrior/WarriorActions.cpp
@@ -12,3 +12,17 @@ bool CastSunderArmorAction::isUseful()
     Aura* aura = botAI->GetAura("sunder armor", GetTarget(), false, true);
     return !aura || aura->GetStackAmount() < 5 || aura->GetDuration() <= 6000;
 }
+
+Value<Unit*>* CastVigilanceAction::GetTargetValue()
+{
+    return context->GetValue<Unit*>("party member without aura", "vigilance");
+}
+
+bool CastVigilanceAction::Execute(Event event)
+{
+    Unit* target = GetTarget();
+    if (!target || target == bot)
+        return false;
+
+    return botAI->CastSpell("vigilance", target);
+}
