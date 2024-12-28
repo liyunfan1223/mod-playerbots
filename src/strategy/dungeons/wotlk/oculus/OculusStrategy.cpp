f488fb446eb5063dbe67dc7067c1b11514642726
kadeshar <kadeshar@gmail.com>
2024-12-29 00:15:32 +0100

    - fixed bug with duplicated action name "drake attack"

--- src/strategy/dungeons/wotlk/oculus/OculusStrategy.cpp
+++ src/strategy/dungeons/wotlk/oculus/OculusStrategy.cpp
@@ -17,7 +17,7 @@ void WotlkDungeonOccStrategy::InitTriggers(std::vector<TriggerNode*> &triggers)
     triggers.push_back(new TriggerNode("group flying",
         NextAction::array(0, new NextAction("occ fly drake", ACTION_NORMAL + 1), nullptr)));
     triggers.push_back(new TriggerNode("drake combat",
-        NextAction::array(0, new NextAction("drake attack", ACTION_NORMAL + 5), nullptr)));
+        NextAction::array(0, new NextAction("occ drake attack", ACTION_NORMAL + 5), nullptr)));
 
     // Varos Cloudstrider
     // Seems to be no way to identify the marked cores, may need to hook boss AI..
