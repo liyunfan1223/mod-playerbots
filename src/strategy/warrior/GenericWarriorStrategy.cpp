c253a32039ea66e2af246c3f7fb32c005d94f773
avirar <luke.thorpe@gmail.com>
2024-12-29 16:09:32 +1100

    Update GenericWarriorStrategy.cpp

--- src/strategy/warrior/GenericWarriorStrategy.cpp
+++ src/strategy/warrior/GenericWarriorStrategy.cpp
@@ -48,10 +48,10 @@ void WarrirorAoeStrategy::InitTriggers(std::vector<TriggerNode*>& triggers)
     // clap on snare target", ACTION_HIGH + 3), nullptr))); triggers.push_back(new TriggerNode("thunder clap",
     // NextAction::array(0, new NextAction("thunder clap", ACTION_HIGH + 10), nullptr)));
     triggers.push_back(new TriggerNode(
-        "light aoe", NextAction::array(0, new NextAction("thunder clap", ACTION_HIGH + 5),
-                                       new NextAction("shockwave", ACTION_HIGH + 4),
-                                       new NextAction("sweeping strikes", ACTION_HIGH + 7),
+        "light aoe", NextAction::array(0, new NextAction("sweeping strikes", ACTION_HIGH + 7),
                                        new NextAction("bladestorm", ACTION_HIGH + 6),
+                                       new NextAction("thunder clap", ACTION_HIGH + 5),
+                                       new NextAction("shockwave", ACTION_HIGH + 4),
                                        // new NextAction("whirlwind", ACTION_HIGH + 2),
                                        new NextAction("demoralizing shout without life time check", ACTION_HIGH + 1),
                                        new NextAction("cleave", ACTION_HIGH), nullptr)));
