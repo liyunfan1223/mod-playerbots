568592f18899e28bc8097ba2d099cc13121ebc4f
Yunfan Li <56597220+liyunfan1223@users.noreply.github.com>
2025-01-13 01:03:27 +0800

    Minor fixes to spell and mount init (#863)
    * Make summon imp non-temporary
    
    * Remove horse mount from troll
    
    * Set facing to during spell casting
    
    * Allow rpg status command
    
    * Init unarmed skill and fix skill clear
    
    * Cast delay after interrupt spell

--- src/strategy/rpg/NewRpgAction.h
+++ src/strategy/rpg/NewRpgAction.h
@@ -24,7 +24,7 @@ protected:
     // const int32 setGrindInterval = 5 * 60 * 1000;
     // const int32 setNpcInterval = 1 * 60 * 1000;
     const int32 statusNearNpcDuration = 5 * 60 * 1000;
-    const int32 statusNearRandomDuration = 2 * 60 * 1000;
+    const int32 statusNearRandomDuration = 5 * 60 * 1000;
     const int32 statusRestDuration = 30 * 1000;
     WorldPosition SelectRandomGrindPos();
     WorldPosition SelectRandomInnKeeperPos();
