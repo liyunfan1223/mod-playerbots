e37f210488dcb6a3957d5fc115a1fb8729b4b284
SaW <swerkhoven@outlook.com>
2025-02-19 09:46:00 +0100

    Update RandomPlayerbotFactory.cpp

--- src/RandomPlayerbotFactory.cpp
+++ src/RandomPlayerbotFactory.cpp
@@ -406,6 +406,7 @@ uint32 RandomPlayerbotFactory::CalculateTotalAccountCount()
     // Avoid creating accounts if both maxRandom & ClassBots are set to zero.
     if (sPlayerbotAIConfig->maxRandomBots == 0 &&
         sPlayerbotAIConfig->addClassAccountPoolSize == 0)
+        return 0;
 
     bool isWOTLK = sWorld->getIntConfig(CONFIG_EXPANSION) == EXPANSION_WRATH_OF_THE_LICH_KING;
 
