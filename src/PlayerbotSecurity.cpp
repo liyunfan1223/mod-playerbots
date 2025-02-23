95c2ba9105b574e6d811c0274c2296cb8a16f16e
Yunfan Li <56597220+liyunfan1223@users.noreply.github.com>
2025-02-24 03:09:59 +0800

    Permission check and readability improvements (#997)
    * AiFactory check by spell instead of level
    
    * Ignore group security for guild invitation

--- src/PlayerbotSecurity.cpp
+++ src/PlayerbotSecurity.cpp
@@ -139,7 +139,7 @@ PlayerbotSecurityLevel PlayerbotSecurity::LevelFor(Player* from, DenyReason* rea
             return PLAYERBOT_SECURITY_INVITE;
         }
 
-        if (group->IsFull())
+        if (!ignoreGroup && group->IsFull())
         {
             if (reason)
                 *reason = PLAYERBOT_DENY_FULL_GROUP;
@@ -147,7 +147,7 @@ PlayerbotSecurityLevel PlayerbotSecurity::LevelFor(Player* from, DenyReason* rea
             return PLAYERBOT_SECURITY_TALK;
         }
 
-        if (group->GetLeaderGUID() != bot->GetGUID())
+        if (!ignoreGroup && group->GetLeaderGUID() != bot->GetGUID())
         {
             if (reason)
                 *reason = PLAYERBOT_DENY_NOT_LEADER;
