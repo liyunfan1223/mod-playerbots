1e817b51ce2a584ede5691a37a22d1ea891e3161
avirar <luke.thorpe@gmail.com>
2025-03-03 13:17:08 +1100

    TellMaster item usage with qi [itemlink]

--- src/strategy/triggers/ChatTriggerContext.h
+++ src/strategy/triggers/ChatTriggerContext.h
@@ -127,6 +127,7 @@ public:
         creators["dps"] = &ChatTriggerContext::dps;
         creators["disperse"] = &ChatTriggerContext::disperse;
         creators["calc"] = &ChatTriggerContext::calc;
+        creators["qi"] = &ChatTriggerContext::qi;
     }
 
 private:
@@ -233,6 +234,7 @@ private:
     static Trigger* dps(PlayerbotAI* ai) { return new ChatCommandTrigger(ai, "dps"); }
     static Trigger* disperse(PlayerbotAI* ai) { return new ChatCommandTrigger(ai, "disperse"); }
     static Trigger* calc(PlayerbotAI* ai) { return new ChatCommandTrigger(ai, "calc"); }
+    static Trigger* qi(PlayerbotAI* botAI) { return new ChatCommandTrigger(botAI, "qi"); }
 };
 
 #endif
