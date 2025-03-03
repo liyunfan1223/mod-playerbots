c8bdb04e41cdab18aa77d3fce034b6425690b5dd
avirar <luke.thorpe@gmail.com>
2025-03-04 09:51:56 +1100

    Only check IsLocked if LockID != 0

--- src/strategy/actions/OpenItemAction.cpp
+++ src/strategy/actions/OpenItemAction.cpp
@@ -58,8 +58,9 @@ bool OpenItemAction::CanOpenItem(Item* item)
     if (!itemTemplate)
         return false;
 
-    // Check if the item has the openable flag
-    return (itemTemplate->Flags & ITEM_FLAG_HAS_LOOT) && (!item->IsLocked());
+    // Check if the item has the openable flag and is not locked
+    return (itemTemplate->Flags & ITEM_FLAG_HAS_LOOT) && 
+       (itemTemplate->LockID == 0 || !item->IsLocked());
 }
 
 void OpenItemAction::OpenItem(Item* item, uint8 bag, uint8 slot)
