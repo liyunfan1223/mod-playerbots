2e0a0f084296b55d250f954a89a3cb37295074d2
avirar <luke.thorpe@gmail.com>
2025-03-04 11:34:20 +1100

    Corrected call to CastSpell

--- src/strategy/actions/UnlockTradedItemAction.cpp
+++ src/strategy/actions/UnlockTradedItemAction.cpp
@@ -85,7 +85,7 @@ void UnlockTradedItemAction::UnlockItem(Item* item)
     }
 
     // Use CastSpell to unlock the item
-    if (botAI->CastSpell(PICK_LOCK_SPELL_ID, bot, false, item))
+    if (botAI->CastSpell(PICK_LOCK_SPELL_ID, bot, item))
     {
         std::ostringstream out;
         out << "Used Pick Lock on: " << item->GetTemplate()->Name1;
