4959c49f089b127338fe8f22900ee9ed31066398
EricksOliveira <ericksoliveira258@gmail.com>
2025-04-02 23:50:51 -0300

    .

--- src/strategy/actions/UseMeetingStoneAction.cpp
+++ src/strategy/actions/UseMeetingStoneAction.cpp
@@ -244,6 +244,6 @@ bool SummonAction::Teleport(Player* summoner, Player* player)
     }
 
     if(summoner != player)
-         botAI->TellError(requester, "Not enough place to summon");
+         botAI->TellError("Not enough place to summon");
     return false;
 }
