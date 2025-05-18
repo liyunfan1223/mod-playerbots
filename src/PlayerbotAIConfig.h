ea122874d28001c74128a9567ef7916c94f2db82
Yunfan Li <lyf_liyunfan@sina.com>
2025-05-18 19:22:27 +0800

    Convert PlayerbotsXPRate to RandomBotXPRate

--- src/PlayerbotAIConfig.h
+++ src/PlayerbotAIConfig.h
@@ -275,7 +275,7 @@ public:
     bool randomBotShowCloak;
     bool randomBotFixedLevel;
     bool disableRandomLevels;
-    float playerbotsXPrate;
+    float randomBotXPRate;
     uint32 randomBotAllianceRatio;
     uint32 randomBotHordeRatio;
     bool disableDeathKnightLogin;
