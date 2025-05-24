1cedb6d76d612c4e610265d4246a4eb19a4c91c6
Yunfan Li <lyf_liyunfan@sina.com>
2025-05-24 22:37:21 +0800

    Active loot roll

--- src/strategy/generic/WorldPacketHandlerStrategy.cpp
+++ src/strategy/generic/WorldPacketHandlerStrategy.cpp
@@ -67,6 +67,9 @@ void WorldPacketHandlerStrategy::InitTriggers(std::vector<TriggerNode*>& trigger
     // quest ?
     //triggers.push_back(new TriggerNode("quest confirm", NextAction::array(0, new NextAction("quest confirm", relevance), nullptr)));
     triggers.push_back(new TriggerNode("questgiver quest details", NextAction::array(0, new NextAction("turn in query quest", relevance), nullptr)));
+
+    // loot roll
+    triggers.push_back(new TriggerNode("very often", NextAction::array(0, new NextAction("loot roll", 10.0f), nullptr)));
 }
 
 WorldPacketHandlerStrategy::WorldPacketHandlerStrategy(PlayerbotAI* botAI) : PassTroughStrategy(botAI)
