02171bb7454c0392bbce45a072083a476ab9f349
Keleborn <22352763+Celandriel@users.noreply.github.com>
2025-07-07 12:02:59 -0700

    First try of fishing with master

---
+++ src/strategy/actions/FishWithMasterAction.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU GPL v2 license, you may redistribute it
+ * and/or modify it under version 2 of the License, or (at your option), any later version.
+ */
+
+#ifndef _PLAYERBOT_FISHWITHMASTERACTION_H
+#define _PLAYERBOT_FISHWITHMASTERACTION_H
+
+#include "Action.h"
+
+class PlayerbotAI;
+
+class FishWithMasterAction : public Action
+{
+public:
+    FishWithMasterAction(PlayerbotAI* botAI) : Action(botAI, "fish with master") {}
+
+    bool Execute(Event event) override;
+};
+
+#endif
