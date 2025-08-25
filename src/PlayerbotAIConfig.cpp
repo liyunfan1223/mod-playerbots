8a43fa8f4c1faef72fbc55be9c016763182a938c
Crow <pengchengw@me.com>
2025-08-25 10:36:58 -0500

    Update source default pvpProhibitedAreaIDs

--- src/PlayerbotAIConfig.cpp
+++ src/PlayerbotAIConfig.cpp
@@ -150,8 +150,10 @@ bool PlayerbotAIConfig::Initialize()
                                            "2255,656,2361,2362,2363,976,35,2268,3425,392,541,1446,3828,3712,3738,3565,"
                                            "3539,3623,4152,3988,4658,4284,4418,4436,4275,4323,4395,3703,4298,3951"),
         pvpProhibitedZoneIds);
-    LoadList<std::vector<uint32>>(sConfigMgr->GetOption<std::string>("AiPlayerbot.PvpProhibitedAreaIds", "976,35"),
-                                  pvpProhibitedAreaIds);
+    LoadList<std::vector<uint32>>(
+        sConfigMgr->GetOption<std::string>("AiPlayerbot.PvpProhibitedAreaIds",
+                                           "976,35,392,2268,4161,4010,4317,4312,3649,3887,3958,3724,4080"),
+        pvpProhibitedAreaIds);
     fastReactInBG = sConfigMgr->GetOption<bool>("AiPlayerbot.FastReactInBG", true);
     LoadList<std::vector<uint32>>(
         sConfigMgr->GetOption<std::string>("AiPlayerbot.RandomBotQuestIds", "7848,3802,5505,6502,7761"),
