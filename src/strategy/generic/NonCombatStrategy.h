1f7ec6e878005c6f21fed90d0ce53bc114dd3a6f
Keleborn <22352763+Celandriel@users.noreply.github.com>
2025-09-03 08:35:54 -0700

    add override to non combat strategy.

--- src/strategy/generic/NonCombatStrategy.h
+++ src/strategy/generic/NonCombatStrategy.h
@@ -64,6 +64,6 @@ public:
     MasterFishStrategy(PlayerbotAI* botAI) : Strategy(botAI){}
     uint32 GetType() const override { return STRATEGY_TYPE_NONCOMBAT; }
     void InitTriggers(std::vector<TriggerNode*>& triggers) override;
-    std::string const getName(){return "masterfishing";}
+    std::string const getName() override {return "masterfishing";}
 };
 #endif
