b8a88ef9b5f018141456d4d5a4ec3b999feb0c5f
wetbrownsauce <you@example.com>
2025-09-03 01:47:07 -0600

    Fix bots sometimes not using drake mount

--- src/strategy/dungeons/wotlk/oculus/OculusActions.cpp
+++ src/strategy/dungeons/wotlk/oculus/OculusActions.cpp
@@ -66,7 +66,7 @@ bool MountDrakeAction::Execute(Event event)
     for (auto& member : members)
     {
         Player* player = botAI->GetPlayer(member);
-        if (!player) { continue; }
+        if (!sPlayerbotsMgr->GetPlayerbotAI(player)) { continue; }
 
         for (int i = 0; i < composition.size(); i++)
         {
